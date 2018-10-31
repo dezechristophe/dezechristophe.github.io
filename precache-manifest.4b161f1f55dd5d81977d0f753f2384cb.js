bb3f7f193397fe1ece8bc8bda7661cb8c130eb11
DEZE Christophe <dezechristophe@gmail.com>
2018-10-31 09:16:20 +0100

    Initial commit

---
+++ precache-manifest.4b161f1f55dd5d81977d0f753f2384cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "da1eaa38473577995c90",
+    "url": "/js/about.8a391d75.js"
+  },
+  {
+    "revision": "4aed663b6ba8205258a3",
+    "url": "/css/app.a07cbf35.css"
+  },
+  {
+    "revision": "4aed663b6ba8205258a3",
+    "url": "/js/app.4a452dda.js"
+  },
+  {
+    "revision": "3fdf7228dfa9306d24c7",
+    "url": "/js/chunk-vendors.9dc9a68c.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e47753c5a2a42720e89d9303f3c29598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
